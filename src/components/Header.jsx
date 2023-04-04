f809375e15909661e7afe4a0cf4eb7b5486e9d4d
Manish Sahni <manishsahni2000@gmail.com>
2023-04-04 12:01:50 +1000

    Adding Tiles template

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,10 @@ import DarkModeSwitch from "./DarkModeSwitch";
 export default function Header() {
   return (
     <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
-      {/* <div className="flex">
+      <div className="flex">
         <MenuItem title="HOME" address="/" Icon={AiFillHome} />
         <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
-      </div> */}
+      </div>
       <div className="flex items-center space-x-5">
         <DarkModeSwitch />
         <Link href="/">
