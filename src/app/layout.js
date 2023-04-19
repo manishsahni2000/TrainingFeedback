ba1dcabf7f11f073683d98fe018dee873fd5a390
Manish Sahni <manishsahni2000@gmail.com>
2023-04-19 11:06:27 +1000

    tring dark theme always on

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,8 +9,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="bg-gray-900">
+    <html lang="en" style={{ colorScheme: 'dark' }}>
+      <body>
         <Providers>
         <Header />
         {children}
